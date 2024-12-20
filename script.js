41f7c4098faaf586da65cda242a4d95e54b49885
KS <144380095+kaustubh2279@users.noreply.github.com>
2024-12-21 01:12:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const navbarLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+  // Toggle mobile menu visibility
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+// Close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Close menu when nav link is clicked
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", () => menuOpenButton.click());
+});
+
+/* Initializing Swiper */
+let swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  /* Responsive breakpoints */
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+// Wait for the DOM to load
+document.addEventListener("DOMContentLoaded", () => {
+  // Select the "Join Us" link
+  const joinUsLink = document.getElementById("join-us-link");
+
+  // Add a click event listener
+  joinUsLink.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent default anchor behavior
+    alert("No New Registrations"); // Show the message
+  });
+});
